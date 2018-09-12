cd4348570608903924ea7ad41ca3d0754772f8db
yapozhong <879728937@qq.com>
2018-09-12 09:43:23 +0800

    Impl reactive mongo's findByCluster

---
+++ simter-operation-data-reactive-mongo/src/test/kotlin/tech/simter/operation/dao/reactive/mongo/OperationDaoImplTest.kt
@@ -0,0 +1,60 @@
+package tech.simter.operation.dao.reactive.mongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import reactor.test.StepVerifier
+import tech.simter.operation.dao.OperationDao
+import tech.simter.operation.dao.reactive.mongo.PoUtil.Companion.randomOperation
+import tech.simter.operation.dao.reactive.mongo.PoUtil.Companion.randomString
+import java.time.OffsetDateTime
+
+/**
+ * Test [OperationDaoImpl]
+ *
+ * @author zh
+ */
+@SpringJUnitConfig(ModuleConfiguration::class)
+@DataMongoTest
+internal class OperationDaoImplTest @Autowired constructor(
+  private val repository: OperationReactiveRepository,
+  private val dao: OperationDao
+) {
+
+  @Test
+  fun findByCluster() {
+    // init data
+    val cluster = randomString()
+    val now = OffsetDateTime.now()
+    val operation1 = randomOperation(cluster = cluster, offsetDateTime = now)
+    val operation2 = randomOperation(cluster = cluster, offsetDateTime = now.minusHours(1))
+
+    // invoke
+    val result = repository
+      .saveAll(listOf(operation1, operation2, randomOperation(cluster = randomString()), randomOperation()))
+      .thenMany(dao.findByCluster(cluster))
+
+    // verify
+    StepVerifier.create(result)
+      .expectNext(operation1)
+      .expectNext(operation2)
+      .verifyComplete()
+  }
+
+  @Test
+  fun findByClusterNothing() {
+    StepVerifier.create(dao.findByCluster(randomString())).verifyComplete()
+  }
+
+  @Test
+  fun findByClusterNotFound() {
+    // init data and invoke
+    val result = repository
+      .saveAll(listOf(randomOperation(cluster = randomString()), randomOperation()))
+      .thenMany(dao.findByCluster(randomString()))
+
+    // verify
+    StepVerifier.create(result).verifyComplete()
+  }
+}
