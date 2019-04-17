18d9e7f6ef9b0c842be22793fdb685f7058b85ce
RJ <rongjihuang@gmail.com>
2019-04-17 09:38:30 +0800

    Polishing webflux - change unit test code to use MockK and SpringMockK

---
+++ simter-operation-rest-webflux/src/test/kotlin/tech/simter/operation/rest/webflux/handler/TestHelper.kt
@@ -0,0 +1,34 @@
+package tech.simter.operation.rest.webflux.handler
+
+import tech.simter.operation.po.Operation
+import tech.simter.operation.po.Operator
+import tech.simter.operation.po.Target
+import java.util.*
+
+/**
+ * provide public method of test
+ *
+ * @author zh
+ * @author RJ
+ */
+object TestHelper {
+  fun randomOperation(cluster: String? = null): Operation {
+    return Operation(
+      type = randomString(),
+      operator = Operator(
+        id = randomString(),
+        name = randomString()
+      ),
+      target = Target(
+        id = randomString(),
+        type = randomString(),
+        name = randomString()
+      ),
+      cluster = cluster
+    )
+  }
+
+  fun randomString(): String {
+    return UUID.randomUUID().toString()
+  }
+}
--- simter-operation-rest-webflux/src/test/kotlin/tech/simter/operation/rest/webflux/handler/operation/FindByClusterHandlerTest.kt
+++ simter-operation-rest-webflux/src/test/kotlin/tech/simter/operation/rest/webflux/handler/operation/FindByClusterHandlerTest.kt
@@ -1,16 +1,16 @@
 package tech.simter.operation.rest.webflux.handler.operation
 
-import com.nhaarman.mockito_kotlin.verify
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import org.springframework.test.web.reactive.server.WebTestClient.bindToRouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions.route
 import reactor.core.publisher.Flux
-import tech.simter.operation.rest.webflux.handler.PoUtil.Companion.randomOperation
-import tech.simter.operation.rest.webflux.handler.PoUtil.Companion.randomString
+import tech.simter.operation.rest.webflux.handler.TestHelper.randomOperation
+import tech.simter.operation.rest.webflux.handler.TestHelper.randomString
 import tech.simter.operation.rest.webflux.handler.operation.FindByClusterHandler.Companion.REQUEST_PREDICATE
 import tech.simter.operation.service.OperationService
 
@@ -21,7 +21,7 @@ import tech.simter.operation.service.OperationService
  * @author RJ
  */
 @SpringJUnitConfig(FindByClusterHandler::class)
-@MockBean(OperationService::class)
+@MockkBean(OperationService::class)
 class FindByClusterHandlerTest @Autowired constructor(
   private val handler: FindByClusterHandler,
   private val service: OperationService
@@ -33,7 +33,7 @@ class FindByClusterHandlerTest @Autowired constructor(
     val cluster = randomString()
     val operation1 = randomOperation(cluster)
     val operation2 = randomOperation(cluster)
-    `when`(service.findByCluster(cluster)).thenReturn(Flux.just(operation1, operation2))
+    every { service.findByCluster(cluster) } returns Flux.just(operation1, operation2)
 
     // invoke
     val response = client.get().uri("/cluster/$cluster").exchange()
@@ -55,6 +55,8 @@ class FindByClusterHandlerTest @Autowired constructor(
       .jsonPath("$[1].target.type").isEqualTo(operation2.target.type)
       .jsonPath("$[1].target.name").isEqualTo(operation2.target.name!!)
       .jsonPath("$[1].cluster").isEqualTo(operation2.cluster!!)
-    verify(service).findByCluster(cluster)
+    verify(exactly = 1) {
+      service.findByCluster(cluster)
+    }
   }
 }
