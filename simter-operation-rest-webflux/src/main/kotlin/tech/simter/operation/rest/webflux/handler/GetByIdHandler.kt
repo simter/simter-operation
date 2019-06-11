c459785e2e953d34e6cb12799c97d01c53bda27f
RJ <rongjihuang@gmail.com>
2019-06-11 17:47:19 +0800

    Implement rest-api'GET/'

---
+++ simter-operation-rest-webflux/src/main/kotlin/tech/simter/operation/rest/webflux/handler/GetByIdHandler.kt
@@ -0,0 +1,35 @@
+package tech.simter.operation.rest.webflux.handler
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.*
+import org.springframework.web.reactive.function.server.RequestPredicates.GET
+import org.springframework.web.reactive.function.server.ServerResponse.noContent
+import reactor.core.publisher.Mono
+import tech.simter.operation.core.Operation
+import tech.simter.operation.core.OperationService
+
+/**
+ * The [HandlerFunction] for get [Operation] by id
+ *
+ * @author zf
+ * @author RJ
+ */
+@Component
+class GetByIdHandler @Autowired constructor(
+  private val operationService: OperationService
+): HandlerFunction<ServerResponse> {
+  override fun handle(request: ServerRequest): Mono<ServerResponse> {
+    val mono = operationService.get( request.pathVariable("id"))
+    return mono.hasElement()
+      .flatMap { if (it) ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(mono)
+      else noContent().build()
+      }
+
+  }
+
+  companion object {
+    val REQUEST_PREDICATE: RequestPredicate = GET("/{id}")
+  }
+}
---
+++ simter-operation-rest-webflux/src/test/kotlin/tech/simter/operation/rest/webflux/handler/GetByIdHandlerTest.kt
@@ -0,0 +1,68 @@
+package tech.simter.operation.rest.webflux.handler
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
+import tech.simter.operation.core.OperationService
+import tech.simter.util.RandomUtils
+import java.time.OffsetDateTime
+import java.time.temporal.ChronoUnit
+
+/**
+ * Test [GetByIdHandler]
+ *
+ * @author zf
+ * @author RJ
+ */
+@SpringJUnitConfig(UnitTestConfiguration::class)
+@MockkBean(OperationService::class)
+@WebFluxTest
+class GetByIdHandlerTest @Autowired constructor(
+  private val client: WebTestClient,
+  private val mapper: ObjectMapper,
+  private val service: OperationService
+) {
+
+  @Test
+  fun `found something`() {
+    // mock
+    val id = RandomUtils.randomString()
+    val now = OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS)
+    val operation = TestHelper.randomOperation(
+      ts = now.minusHours(1),
+      items = setOf(TestHelper.randomOperationItem(id = "field1"), TestHelper.randomOperationItem(id = "field2"))
+    ) // with items
+
+    every { service.get(id) } returns Mono.just(operation)
+    val responseBody = mapper.writeValueAsString(operation)
+
+    // invoke
+    val response = client.get().uri("/$id").exchange()
+    // verify
+    response.expectStatus().isOk
+      .expectBody()
+      .json(responseBody)
+    verify(exactly = 1) {service.get(id)  }
+
+  }
+  @Test
+  fun `found nothing`() {
+    // mock
+    val id = RandomUtils.randomString()
+    every { service.get(id) } returns Mono.empty()
+
+    // invoke and verify
+    client.get().uri("/$id")
+      .exchange()
+      .expectStatus().isNoContent
+      .expectBody().isEmpty
+
+  }
+}
