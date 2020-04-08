72237cb26e1499a73657d9ae7b83ba34e2b13458
RJ <rongjihuang@gmail.com>
2020-04-09 00:07:23 +0800

    Rename property 'module.rest-context-path.simter-operation' to 'simter-operation.rest-context-path'

--- simter-operation-rest-webflux/src/main/kotlin/tech/simter/operation/rest/webflux/ModuleConfiguration.kt
+++ simter-operation-rest-webflux/src/main/kotlin/tech/simter/operation/rest/webflux/ModuleConfiguration.kt
@@ -1,6 +1,5 @@
 package tech.simter.operation.rest.webflux
 
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.Page
-import org.springframework.http.MediaType.TEXT_PLAIN
 import org.springframework.web.reactive.function.server.router
 import tech.simter.operation.PACKAGE
 import tech.simter.operation.rest.webflux.handler.*
@@ -17,7 +15,7 @@ import tech.simter.operation.rest.webflux.handler.*
  * All configuration for this module.
  *
  * Register a `RouterFunction<ServerResponse>` with all routers for this module.
- * The default context-path of this router is '/operation'. And can be config by property `module.rest-context-path.simter-operation`.
+ * The default context-path of this router is '/operation'. And can be config by property `simter-operation.rest-context-path`.
  *
  * @author RJ
  * @author zh
@@ -25,21 +23,13 @@ import tech.simter.operation.rest.webflux.handler.*
 @Configuration("$PACKAGE.rest.webflux.ModuleConfiguration")
 @ComponentScan
 class ModuleConfiguration @Autowired constructor(
-  @Value("\${module.version.simter-operation:UNKNOWN}") private val version: String,
-  @Value("\${module.rest-context-path.simter-operation:/operation}") private val contextPath: String,
+  @Value("\${simter-operation.rest-context-path:/operation}") private val contextPath: String,
   private val findHandler: FindHandler,
   private val findByBatchHandler: FindByBatchHandler,
   private val findByTargetHandler: FindByTargetHandler,
   private val createHandler: CreateHandler,
   private val getByIdHandler: GetByIdHandler
 ) {
-  private val logger = LoggerFactory.getLogger(ModuleConfiguration::class.java)
-
-  init {
-    logger.warn("module.version.simter-operation='{}'", version)
-    logger.warn("module.rest-context-path.simter-operation='{}'", contextPath)
-  }
-
   /** Register a `RouterFunction<ServerResponse>` with all routers for this module */
   @Bean("$PACKAGE.rest.webflux.Routes")
   @ConditionalOnMissingBean(name = ["$PACKAGE.rest.webflux.Routes"])
@@ -55,8 +45,6 @@ class ModuleConfiguration @Autowired constructor(
       GetByIdHandler.REQUEST_PREDICATE.invoke(getByIdHandler::handle)
       // POST / create Operation
       CreateHandler.REQUEST_PREDICATE.invoke(createHandler::handle)
-      // GET /
-      GET("/") { ok().contentType(TEXT_PLAIN).bodyValue("simter-operation-rest-webflux-$version") }
     }
   }
 }
