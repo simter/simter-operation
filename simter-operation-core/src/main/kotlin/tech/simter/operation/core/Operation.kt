bf9c1bbbe1aa36ded6c91e616e4361b2ef366139
RJ <rongjihuang@gmail.com>
2020-06-05 15:35:15 +0800

    Move ImmutableOperation to Operation.Impl

--- simter-operation-core/src/main/kotlin/tech/simter/operation/core/Operation.kt
+++ simter-operation-core/src/main/kotlin/tech/simter/operation/core/Operation.kt
@@ -1,6 +1,7 @@
 package tech.simter.operation.core
 
 import java.time.OffsetDateTime
+import java.util.*
 
 /**
  * The Operation.
@@ -15,6 +16,7 @@ interface Operation {
 
   /** The operated date-time */
   val ts: OffsetDateTime
+
   /** The operated type. Such as Creation, Modification, Deletion */
   val type: String
 
@@ -33,9 +35,11 @@ interface Operation {
   /** The optional operated remark */
   val remark: String?
     get() = null
+
   /** The optional operated result. Such as 'ok', 'not ok' */
   val result: String?
     get() = null
+
   /** The optional batch */
   val batch: String?
     get() = null
@@ -53,17 +57,56 @@ interface Operation {
    */
   interface Item {
     val id: String
+
     /** item title, label or name */
     val title: String?
       get() = null
+
     /** value type, eg: String|JsonObject|JsonArray */
     val valueType: String
+
     /** The json string of old value */
     val oldValue: String?
       get() = null
+
     /** The json string of new value */
     val newValue: String?
       get() = null
+
+    /** An inner immutable [Item] implementation */
+    data class Impl(
+      override val id: String,
+      override val title: String? = id,
+      override val valueType: String,
+      override val oldValue: String? = null,
+      override val newValue: String? = null
+    ) : Item {
+      companion object {
+        fun from(item: Item): Impl {
+          return if (item is Impl) item
+          else Impl(
+            id = item.id,
+            title = item.title,
+            valueType = item.valueType,
+            oldValue = item.oldValue,
+            newValue = item.newValue
+          )
+        }
+      }
+    }
+
+    companion object {
+      /** Create an immutable [Item] instance */
+      fun of(
+        id: String,
+        title: String? = id,
+        valueType: String,
+        oldValue: String? = null,
+        newValue: String? = null
+      ): Item {
+        return Impl(id, title, valueType, oldValue, newValue)
+      }
+    }
   }
 
   /**
@@ -88,6 +131,46 @@ interface Operation {
         }
         throw IllegalArgumentException("Unsupported Operation Type value: $value")
       }
+    }
+  }
+
+  /** An inner immutable [Operation] implementation */
+  data class Impl(
+    override val id: String = UUID.randomUUID().toString(),
+    override val ts: OffsetDateTime = OffsetDateTime.now(),
+    override val type: String,
+    override val operatorId: String,
+    override val operatorName: String,
+    override val targetId: String,
+    override val targetType: String,
+    override val title: String? = null,
+    override val remark: String? = null,
+    override val result: String? = null,
+    override val batch: String? = null,
+    override val items: Set<Item.Impl> = emptySet()
+  ) : Operation
+
+  companion object {
+    /** Create an immutable [Operation] instance */
+    fun of(
+      id: String = UUID.randomUUID().toString(),
+      ts: OffsetDateTime = OffsetDateTime.now(),
+      type: String,
+      operatorId: String,
+      operatorName: String,
+      targetId: String,
+      targetType: String,
+      title: String? = null,
+      remark: String? = null,
+      result: String? = null,
+      batch: String? = null,
+      items: Set<Item> = emptySet()
+    ): Operation {
+      return Impl(
+        id, ts, type, operatorId, operatorName,
+        targetId, targetType, title,
+        remark, result, batch, items.map { Item.Impl.from(it) }.toSet()
+      )
     }
   }
 }
